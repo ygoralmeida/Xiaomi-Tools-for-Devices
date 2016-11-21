12c9da4e6b5222efe2447becb54405abb0776e73
Francisco Eduardo <francisco.mont@outlook.com.br>
2016-11-21 07:41:26 -0300

    Add files via upload

--- iniciar.bat
+++ iniciar.bat
@@ -229,7 +229,7 @@ Echo  읕컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴켸
 Echo.
 
 7za.exe -aoa e %rom%\Mi4C.tgz -o%firmware% >%logs%\7zip.txt 2>&1
-7za.exe -aoa e %firmware%\Mi4C.tgz -o%firmware%\miui >%logs%\7zip.txt 2>&1
+7za.exe -aoa e %firmware%\Mi4C.tar -o%firmware%\miui >%logs%\7zip.txt 2>&1
 
 cls
 Echo.
